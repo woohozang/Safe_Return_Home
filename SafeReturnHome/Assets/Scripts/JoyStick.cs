612703308588d84780a3f973d6fdbc298e7ac7af
woohozang <woohozang@naver.com>
2025-05-12 01:23:26 +0900

    조이스틱, 속도 증가 구현
    큐브로 테스트

---
+++ SafeReturnHome/Assets/Scripts/JoyStick.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyStick : MonoBehaviour
+{
+    public FixedJoystick joystick;
+    
+    public float baseSpeed = 30f; //기본 속도
+    public float boost = 2f;
+    private bool isBoosted = false;
+    
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = isBoosted ? baseSpeed * boost : baseSpeed;
+        Debug.Log($"Speed: {speed}, isBoosted: {isBoosted}");
+
+        Vector3 direction = new Vector3(joystick.Horizontal, 0f, joystick.Vertical);
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+    public void SetBoostedSpeed(bool boosted)
+    {
+        isBoosted = boosted;
+        //Debug.Log("Boosted: " + isBoosted);
+    }
+}
---
+++ SafeReturnHome/Assets/Scripts/SpeedUpBtn.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SpeedUpBtn : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public JoyStick speedbtn;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Down");
+        speedbtn.SetBoostedSpeed(true);
+       
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Up");
+        speedbtn.SetBoostedSpeed(false);
+    }
+    
+}
