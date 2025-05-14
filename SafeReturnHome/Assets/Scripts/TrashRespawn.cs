e32e82998a06bd0fa16face514b58f3039b56b6a
woohozang <woohozang@naver.com>
2025-05-14 22:46:02 +0900

    다양한 종류 쓰레기 떨어지게끔 구현
    배열 리스트 알았다 재미따

--- SafeReturnHome/Assets/Scripts/TrashRespawn.cs
+++ SafeReturnHome/Assets/Scripts/TrashRespawn.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class TrashRespawn : MonoBehaviour
 {
-    public GameObject trashPrefab; // 생성할 쓰레기 프리팹
-    //public GameObject trashCanPrefab;
+    public GameObject[] trashPrefab; // 생성할 쓰레기 프리팹 배열 선언
+    List<GameObject> trashList = new List<GameObject>(); //쓰레기 프리팹 리스트(종류)
+
     public Vector2 spawnRangeX = new Vector2(-10f, 10f); // X축 범위
     public Vector2 spawnRangeZ = new Vector2(-10f, 10f); // Z축 범위
     public float spawnHeight = 0.5f; // 쓰레기 생성 높이
     public float spawnInterval = 3.0f; // 생성 간격 (초)
-    //public int count = 1;
     private float nextSpawnTime = 0.0f;
 
     void Update()
@@ -16,7 +18,7 @@ public class TrashRespawn : MonoBehaviour
         if (Time.time >= nextSpawnTime)
         {
             nextSpawnTime = Time.time + spawnInterval;
-            int count = Random.Range(1, 2);
+            int count = Random.Range(1, 3); //랜덤 쓰레기가 낙하할 때 1개 또는 2개동시에 떨어짐
             
             for (int i = 0; i < count; i++)
             {
@@ -27,18 +29,19 @@ public class TrashRespawn : MonoBehaviour
 
     void SpawnTrash()
     {
+        if (trashPrefab==null || trashPrefab.Length == 0) //예외처리 방어코드? 인스펙터 창에서 배열 값을 0으로 초기화 하면 예외처리 발생
+        {
+            return;
+        }
         float randomX = Random.Range(spawnRangeX.x, spawnRangeX.y);
         float randomZ = Random.Range(spawnRangeZ.x, spawnRangeZ.y);
         Vector3 spawnPos = new Vector3(randomX, spawnHeight, randomZ);
-
-        if (trashPrefab != null)
+        int TrashIndex = Random.Range(0, trashPrefab.Length);//인스펙터 창에서 동적으로 종류를 설정 후 프리팹 배열의 길이만큼 랜덤하게 쓰레기 투하
+        GameObject AllTrash = trashPrefab[TrashIndex];
+        if(AllTrash != null)
         {
-            Instantiate(trashPrefab, spawnPos, Quaternion.identity);
+            Instantiate(AllTrash, spawnPos, Quaternion.identity);
         }
-       
-        else
-        {
-            Debug.LogWarning("Trash prefab is not assigned in the Inspector.");
-        }
+        
     }
 }
