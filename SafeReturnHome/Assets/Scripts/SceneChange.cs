82e770f4f557693163152f6f160b4aa02d1f4888
woohozang <woohozang@naver.com>
2025-06-10 23:26:03 +0900

    씬전환

--- SafeReturnHome/Assets/Scripts/SceneChange.cs
+++ SafeReturnHome/Assets/Scripts/SceneChange.cs
@@ -6,20 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    public Button StartBtn;
-    //public Button ResetBtn;
+    
     // Start is called before the first frame update
     public void SceneNext()
     {
         SceneManager.LoadScene("GameScene");
     }
-
-    // Update is called once per frame
-    void Update()
+    public void ScenePrevious()
     {
-        if (StartBtn != null)
-        {
-            StartBtn.onClick.AddListener(SceneNext);
-        }
+        SceneManager.LoadScene("FirstScene");
     }
+    // Update is called once per frame
+    
 }
